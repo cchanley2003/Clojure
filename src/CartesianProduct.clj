ea02df08d0587e0890b4b1dad99a160ecd96cab1
cchanley2003 <cchanley2003@yahoo.com>
2015-12-19 11:18:32 -0700

    New problems.

--- src/CartesianProduct.clj
+++ src/CartesianProduct.clj
@@ -1,1 +1,20 @@
  (ns CartesianProduct)
+
+
+ (println "one")
+
+(def input1 #{"ace" "king" "queen"})
+
+ (def input2 #{"♠" "♥" "♦" "♣"})
+
+ (println input2)
+
+ (defn cart [x y] (loop [toMap (first x) result #{} toDo (rest x)]
+                   (if (empty? toDo)
+                     (into result (map (fn [z] (vector toMap z)) y))
+                    (recur
+                     (first toDo)
+                     (into result (map (fn [z] (vector toMap z)) y))
+                     (rest toDo)))))
+
+ (println (cart input1 input2))
