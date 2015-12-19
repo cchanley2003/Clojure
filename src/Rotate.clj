7ac1b0593910f381d1287ba84d046c592841bbf8
cchanley2003 <cchanley2003@yahoo.com>
2015-12-19 13:55:39 -0700

    New problems.

--- src/Rotate.clj
+++ src/Rotate.clj
@@ -1,1 +1,15 @@
  (ns Rotate)
+
+
+ (defn rotate [amount list]
+   (if (= amount 0)
+     list
+     (if (< amount 0)
+       (rotate (inc amount) (cons  (last list) (drop-last list)) )
+       (rotate (dec amount) (conj (rest list) (first list))))))
+
+(def input1 [1 2 3 4 5])
+
+(println (rotate 2 input1))
+
+
