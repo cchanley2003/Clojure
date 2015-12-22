b03a41282e6a250c14d0562f4faaecd7d603d38d
chanley2003 <cchanley2003@yahoo.com>
2015-12-22 13:01:25 -0700

    Count occurs

--- src/CountOccurs.clj
+++ src/CountOccurs.clj
@@ -1,1 +1,12 @@
  (ns CountOccurs)
+
+ (def input1 [1 1 2 3 2 1 1])
+
+ (def f (group-by identity input1))
+
+ (println (apply merge (map  #(assoc {} (count %) (first %)) (vals f))))
+
+(defn co [input] (apply merge (map #(assoc {} (first %) (count %)) (vals (group-by identity input)))))
+
+(println (co input1))
+
