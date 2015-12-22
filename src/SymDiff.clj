776298f6e4d2a7797f89677e0e21fd39900e11cf
chanley2003 <cchanley2003@yahoo.com>
2015-12-22 14:40:02 -0700

    Start of Dot Product, Finished Sym Diff

--- src/SymDiff.clj
+++ src/SymDiff.clj
@@ -1,1 +1,12 @@
  (ns SymDiff)
+
+ (require 'clojure.set)
+
+
+ (println (clojure.set/difference  #{1 3 5 7} #{1 2 3 4 5 6} ))
+
+
+ (defn symDiff [x y] (clojure.set/union (clojure.set/difference x y) (clojure.set/difference y x)))
+
+
+ (println (symDiff  #{1 3 5 7} #{1 2 3 4 5 6}))
