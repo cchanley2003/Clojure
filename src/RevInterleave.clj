db790d94abff43435a1b70e246bf532eb1ad83c8
chanley2003 <cchanley2003@yahoo.com>
2015-12-22 12:09:38 -0700

    Iterate done!

--- src/RevInterleave.clj
+++ src/RevInterleave.clj
@@ -1,1 +1,10 @@
  (ns RevInterleave)
+
+
+(defn revInter [x y] (if (= y 0) x (concat (list (take-nth y x)) (revInter (flatten (map #(rest %) (partition y x))) (dec y)))))
+
+
+ (println (take-nth 3 (range 9)))
+ (println  (take-nth 2 (flatten (map #(rest %) (partition 3 (range 9))))))
+
+ (println (revInter (range 9 ) 3))
