724cf340b0d4daf73ebf5fd028d1f0a2dba3fd58
cchanley2003 <cchanley2003@yahoo.com>
2015-12-26 16:33:44 -0700

    Word sorting done. Start of prime numbers.

--- src/PrimeNumbers.clj
+++ src/PrimeNumbers.clj
@@ -1,1 +1,26 @@
  (ns PrimeNumbers)
+
+
+ (defn isPrime [x]
+   (if (<= x 1)
+     false
+     (if (<= x 3) true
+                  (if  (or (= (rem x 2) 0) (= (rem x 3) 0))
+                    false
+                    (loop [i 5]
+                      (if (> (* i i) x)
+                        true
+                        (if (or (= (rem x i) 0) (= (rem x (+ i 2)) 0))
+                          false
+                          (recur (+ i 6))
+                                    )))))))
+
+
+ (println (isPrime 2))
+ (println (isPrime 5))
+ (println (isPrime 11))
+ (println (isPrime 10))
+
+ (defn primeNums [n] (take  n (filter #(isPrime %) (range))) )
+
+ (println (primeNums 2))
