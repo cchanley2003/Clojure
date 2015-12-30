4a3904d0759bc5daf617dcb263347d6fbcdc48dd
chanley2003 <cchanley2003@yahoo.com>
2015-12-30 11:18:42 -0700

    Finished first hard (longest sub seq). Start of perfect squares.

--- src/LongSubSeq.clj
+++ src/LongSubSeq.clj
@@ -1,6 +1,15 @@
  (ns LongSubSeq)
 
 
+(defn longestSub [x] (reduce #(if (= 1 (count %2)) %1 (if (> (count %1) (count %2)) %1 %2)) '() (loop [result [] currentSeq [(first x)]toDo (rest x)] (if (empty? toDo)
+                                                              (conj result currentSeq)
+                                                               (if (= (first toDo) (inc (last currentSeq)))
+                                                                 (recur result (conj currentSeq (first toDo)) (rest toDo))
+                                                                 (recur (conj result currentSeq) [(first toDo)] (rest toDo)))))))
+
+(println (longestSub [1 0 1 2 3 0 4 5]))
+
+(println (longestSub [7 6 5 4]))
 
 
 
