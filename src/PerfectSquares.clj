9c7affca2762e0508a0ff1511c96cc90db845654
chanley2003 <cchanley2003@yahoo.com>
2015-12-30 11:47:52 -0700

    Perfect squares.

--- src/PerfectSquares.clj
+++ src/PerfectSquares.clj
@@ -1,1 +1,14 @@
  (ns PerfectSquares)
+
+
+ (println (Math/sqrt 16))
+
+ (println (= (Math/sqrt 10) (int (Math/sqrt 10))))
+
+(println (= (Math/sqrt 16) (double (int (Math/sqrt 16)))))
+
+(defn  squares [x] (clojure.string/join "," (filter #(= 0 (compare (Math/sqrt %) (int (Math/sqrt %)))) (map #(read-string %) (clojure.string/split x #",")))))
+
+(println (clojure.string/join "," (squares "1,2,5,16,25,30")))
+
+(println (map #(read-string %) (clojure.string/split "1,4,5,7,16,25" #",")))
