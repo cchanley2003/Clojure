53b4d77882e0fb2ecbadc52bc39adb0964bffa9d
chanley2003 <cchanley2003@yahoo.com>
2016-02-08 16:38:21 -0700

    New Reductions done!

--- src/SeqRed.clj
+++ src/SeqRed.clj
@@ -1,1 +1,18 @@
  (ns SeqRed)
+
+ (println (+ 1))
+
+ (println (empty? (rest [1])))
+
+ (println (reduce + [1]))
+
+ (println (second [1]))
+ (println (lazy-seq [1]))
+
+ (defn newReductions
+   ([f c] (newReductions f (first c) (rest c)))
+   ([f i c] (cond
+               (empty? c) (lazy-seq [i])
+               :else (lazy-cat (lazy-seq [i]) (newReductions f (f i (first c)) (rest c))))))
+
+(println (newReductions + [1 2 3]))
