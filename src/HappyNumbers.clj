3e46dceb66320c11533435b8656950237d5ac06d
chanley2003 <cchanley2003@yahoo.com>
2016-02-09 12:14:56 -0700

    Happy Numbers done!

--- src/HappyNumbers.clj
+++ src/HappyNumbers.clj
@@ -1,1 +1,25 @@
- (ns HappyNumbers)
+(ns HappyNumbers)
+
+(println (str 16))
+
+(println (map #(Integer/parseInt (str %)) (seq (str 16))))
+
+(defn intToDigits2 [x] (map #(Integer/parseInt (str %)) (seq (str x))))
+
+
+(defn HappyNumbers [n] (println n) (if (= n 1) true (HappyNumbers (reduce + (map #(* % %) (intToDigits2 n))))))
+
+(defn newHappy [n] (letfn
+                     [(hNum [x] (reduce + (map #(* % %) (intToDigits x))))
+                      (intToDigits [x] (map #(Integer/parseInt (str %)) (seq (str x))))]
+                     (loop
+                       [prev #{}
+                        initial (hNum n)]
+                       (cond
+                         (= initial 1) true
+                         (contains? prev initial) false
+                         :else (recur (conj prev initial) (hNum initial))))))
+
+;(println (HappyNumbers 3))
+
+(println (newHappy 3))
