c06ccb7b3e234e514d0715c0ab8834ebf34d988b
chanley2003 <cchanley2003@yahoo.com>
2016-02-11 12:09:02 -0700

    Redid Trampoline.

--- src/NewTrampoline.clj
+++ src/NewTrampoline.clj
@@ -1,1 +1,20 @@
  (ns NewTrampoline)
+
+(require 'clojure.test)
+
+(defn newTramp [f & v ]
+  (if (empty? v)
+    (if (fn? f)
+      (newTramp (f)) f)
+    (newTramp (apply f v))))
+
+
+(println 1)
+
+ (println (letfn [(triple [x] #(sub-two (* 3 x)))
+                  (sub-two [x] #(stop?(- x 2)))
+                  (stop? [x] (if (> x 50) x #(triple x)))]
+            (newTramp triple 2)))
+
+(println 2)
+
