ae751c56deb36fc56571235d92dbd45c8782e331
chanley2003 <cchanley2003@yahoo.com>
2016-07-22 10:26:33 -0600

    Did compute machine.

---
+++ src/Intervals.clj
@@ -0,0 +1,21 @@
+ (ns Intervals)
+
+(def input [1 1 2 3 6 5 8])
+
+(defn getEndPoints [x] (letfn [(getIntervals [is]
+                                 (loop [res [] todo is]
+                                   (if (empty? todo)
+                                     res
+                                     (let [intvlTuple (getInterval todo)] (recur (conj res (first intvlTuple)) (second intvlTuple))))))
+                               (getInterval [i]
+                                 (loop [newInterval [] remainder i] (cond
+                                                                      (empty? remainder) (list newInterval remainder)
+                                                                      (empty? newInterval) (recur (conj newInterval (first remainder)) (rest remainder))
+                                                                      (= (inc (last newInterval)) (first remainder)) (recur (conj newInterval (first remainder)) (rest remainder))
+                                                                      :default (list newInterval remainder))))]
+                         (map #(if (> (count %) 1)
+                                [(first %) (second %)]
+                                [(first %)])
+                              (getIntervals (sort (into #{} x))))))
+
+(println (getEndPoints input))
