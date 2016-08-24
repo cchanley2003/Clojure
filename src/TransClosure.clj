1907c8ba405884f0a1b54dcb4ae93307f5266f93
chanley2003 <cchanley2003@yahoo.com>
2016-08-24 13:49:22 -0600

    Trans working.

--- src/TransClosure.clj
+++ src/TransClosure.clj
@@ -8,17 +8,26 @@
 (println (map #(vector 27 (second %1)) (filter #(= (first %1) 9) '([8 4] [4 2] [9 3]))))
 
 
-(defn nextOrder [x]
-  (loop [res x todo x]
-    (let [[a b] (first todo) r (rest todo)]
-      (println res a b)
-      (if
-        (empty? todo)
-        res
-        (recur (clojure.set/union res (set (map #(vector a (second %1)) (filter #(= (first %1) b) x)))) r)))))
 
 
 
-;(println (nextOrder input))
+(defn finalSolution [i] (letfn [(nextOrder [x]
+                                  (loop [res x todo x]
+                                    (let [[a b] (first todo) r (rest todo)]
+                                      (if
+                                        (empty? todo)
+                                        res
+                                        (recur (clojure.set/union res (set (map #(vector a (second %1)) (filter #(= (first %1) b) x)))) r)))))
+                                ]
+                          (loop
+                            [ latest i]
+                            (let
+                              [c (count latest)
+                               new (nextOrder latest)
+                               newCount (count new)]
+                              (if (= c newCount)
+                                latest
+                                (recur new))))))
+(println (finalSolution input))
 
-(println (nextOrder input2))
+(println (finalSolution input2))
