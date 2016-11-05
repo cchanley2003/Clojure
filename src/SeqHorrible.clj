d8d14f5139c61dba1ff891706591f99465c83ffd
chanley2003 <cchanley2003@yahoo.com>
2016-11-05 13:34:09 -0600

    Got tricky card games working

--- src/SeqHorrible.clj
+++ src/SeqHorrible.clj
@@ -4,12 +4,10 @@
 
 (defn shorrible
   ([num s] (shorrible num s 0))
-  ([num s sum]   (loop [todo s sum sum res []] (println " todo " (empty? todo) todo)
-                                               (cond (number? (first todo)) (cond (> (+ sum (first todo)) num) res
+  ([num s sum]   (loop [todo s sum sum res []] (cond (number? (first todo)) (cond (> (+ sum (first todo)) num) res
                                                                                   :else (recur (next todo) (+ sum (first todo)) (conj res (first todo))))
                                                      (empty? todo) res
                                                      :else (let [subseq (shorrible num (first todo) sum)]
-                                                             (println "sub seq " subseq)
                                                              (recur (next todo) (apply + sum subseq) (conj res subseq )))
                                             ))
   ))
@@ -17,9 +15,9 @@
 
 
 
-(println (shorrible 7 [1 2 3]))
+ (println (shorrible 7 [1 2 3]))
 
  (println (shorrible 9 (range)))
 
-;(println (shorrible 5 [1 2 [1 4]]))
+(println (shorrible 5 [1 2 [1 4]]))
 
