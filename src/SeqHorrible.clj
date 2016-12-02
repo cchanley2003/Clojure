a1f8aff5f01555960247fc496d01f900b41948cf
chanley2003 <cchanley2003@yahoo.com>
2016-12-02 13:39:25 -0700

    Fixed horrible seqs

--- src/SeqHorrible.clj
+++ src/SeqHorrible.clj
@@ -3,13 +3,13 @@
 
 
 (defn shorrible
-  ([num s] (shorrible num s 0))
-  ([num s sum]   (seq (loop [todo s sum sum res []] (cond (number? (first todo)) (cond (> (+ sum (first todo)) num) res
+  ([num s] (shorrible num 0 s))
+  ([num sum s]   (loop [todo s sum sum res []] (cond (number? (first todo)) (cond (> (+ sum (first todo)) num) res
                                                                                   :else (recur (next todo) (+ sum (first todo)) (conj res (first todo))))
                                                      (empty? todo) res
-                                                     :else (let [subseq (shorrible num (first todo) sum)]
-                                                             (recur (next todo) (apply + sum subseq) (conj res subseq )))
-                                            )))
+                                                     :else (let [subseq (shorrible num sum (first todo))]
+                                                             (recur (next todo) (apply + sum (flatten subseq)) (conj res subseq )))
+                                            ))
   ))
 
 
@@ -20,4 +20,9 @@
  (println (shorrible 9 (range)))
 
 (println (shorrible 5 [1 2 [1 4]]))
+
+(println (shorrible  10 [1 2 [3 [4 5] 6] 7]))
+
+(println (=  (shorrible 10 [1 2 [3 [4 5] 6] 7])
+    '(1 2 (3 (4)))))
 
