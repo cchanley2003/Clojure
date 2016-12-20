e9fac98d85c90a7dd0c56cd30fa3761bbe1fccea
chanley2003 <cchanley2003@yahoo.com>
2016-12-20 15:29:43 -0700

    Getting there with word chains.

--- src/WordChain.clj
+++ src/WordChain.clj
@@ -1,1 +1,51 @@
  (ns WordChain)
+
+
+(def chain #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"})
+ ;find children for any word
+
+(defn diffSameSize [w1 w2]  (apply + (map #(if (= %1 %2) 0 1) w1 w2)))
+
+(defn diffDiffSize [w1 w2] (loop [todo1 w1 todo2 w2 skipped false] (cond
+                                                                     (or (empty? todo1) (empty? todo2)) true
+                                                                     (= (first todo1) (first todo2)) (recur (rest todo1) (rest todo2) skipped)
+                                                                     skipped false
+                                                                     :else (let [longer (if (> (count todo1) (count todo2)) todo1 todo2)
+                                                                           shorter (if (> (count todo1) (count todo2)) todo2 todo1)] (recur (rest longer) shorter true))
+                                                                     )
+                                                                   )
+  )
+
+(defn isChild? [word candidate] (let [wordSeq (seq word)
+                                      candSeq  (seq candidate)
+                                      lengthDiff (Math/abs (- (count wordSeq) (count candSeq))) ]
+                                  (cond
+                                    (= word candidate) false
+                                    (> lengthDiff 2) false
+                                    (= 0 lengthDiff ) (< (diffSameSize wordSeq candSeq) 2)
+                                    :else (diffDiffSize word candidate)
+                                    )))
+(defn findChildren [word words] (loop [res '() todo words] (cond
+                                                             (empty? todo) res
+                                                             (isChild? word (first todo)) (recur (cons (first todo) res) (rest todo))
+                                                             :else (recur res (rest todo))
+                                                             )))
+
+(defn findAllChildren [words]  (apply merge
+                                      (map
+                                        #(hash-map % (findChildren % words))
+                                        words)))
+
+
+(defn walkChain [word chainMap] (loop [visited #{word} ]))
+
+(defn canChain? [lw] (let [wordsWithChildren (findAllChildren lw)]
+                       (apply or (map #(walkChain % wordsWithChildren) lw)))
+  )
+
+
+;(println (map #({% 1}) chain))
+
+(println (findAllChildren chain))
+
+
